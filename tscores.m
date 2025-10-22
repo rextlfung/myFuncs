bd9a92e5ddb7e22508535da7a706825d90e52581
Rex Fung <rexfung@umich.edu>
2025-10-22 16:12:53 -0400

    Oct 22nd, 2025: reduce memory usage for t-score computation

--- tscores.m
+++ tscores.m
@@ -17,8 +17,7 @@ function T = tscores(img, GLM, c)
     beta_img = img_vec * pinv(GLM).';
     r_img = img_vec - beta_img * GLM.';
     nu = size(GLM,1) - rank(GLM);
-    sigma2 = (r_img * r_img.') / nu;
-    var_cbeta = diag(sigma2) * (c.' / (GLM.' * GLM) * c);
+    var_cbeta = sum(r_img.^2, 2) / nu * (c.' / (GLM.' * GLM) * c);
     T_vec = (beta_img * c) ./ (sqrt(var_cbeta) + eps);
 
     T = reshape(T_vec, sz(1:end-1));
