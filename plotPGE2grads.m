f969243981c06eb188f6ecd8e8f4ef1f7dc6174f
Rex Fung <rexfung@umich.edu>
2026-06-09 15:46:56 -0400

    Adding new custom functions

---
+++ plotPGE2grads.m
@@ -0,0 +1,193 @@
+function W = plotPGE2grads(ceq, sys, varargin)
+% function W = plotPGE2grads(ceq, sys, varargin)
+%
+% Plot Ceq sequence object
+%
+% Inputs:
+%   ceq     struct       Ceq sequence object, see seq2ceq.m
+%   sys     struct       System hardware info, see pge2.getsys()
+%
+% Input options:
+%  timeRange       [2]             Requested start and end times (sec). Actual plot will end on a segment boundary.
+%  blockRange      [2]             Requested start and end blocks. Actual plot will end on a segment boundary.
+%  showBlocks      TRUE/false      Draw vertical lines at block boundaries (default: true)
+%  rotate          true/FALSE      If false, display gradients in logical coordinate frame, i.e., 
+%                                  before rotating. If true, only interpolated gradients are shown.
+%  interpolate     true/FALSE      Interpolate to 4us, or display corner points (for extended traps)
+%  wt              [3]             PNS x/y/z/ channel weights. Default: [1 1 1].
+%
+% Output: 
+%  W               struct containing the plotted waveforms
+
+arg.timeRange = [0 ceq.duration];
+arg.blockRange = [1 ceq.nMax];
+arg.showBlocks = true; 
+arg.rotate = false;
+arg.interpolate = false;
+arg.wt = [1 1 1];
+
+arg = vararg_pair(arg, varargin);   % in ../
+
+if arg.rotate
+    if ~arg.interpolate
+        warning('Gradient waveforms interpolated to 4us');
+    end
+    arg.interpolate = true;
+end
+
+nSubPlots = 3;
+
+if arg.timeRange(1) > ceq.duration
+    error('Request time exceeds sequence duration');
+end
+
+% get peak waveform amplitudes (for setting display range)
+yLim.gx = max(abs(ceq.loop(:,6)))/sys.gamma/100;   % Gauss/cm
+yLim.gy = max(abs(ceq.loop(:,8)))/sys.gamma/100;   % Gauss/cm
+yLim.gz = max(abs(ceq.loop(:,10)))/sys.gamma/100;  % Gauss/cm
+
+% Loop through sequence segments and display 
+W.tic = [];  % block boundary times
+tStart = 0;  % start of plot
+W.gx.t = 0; W.gx.signal = 0;
+W.gy.t = 0; W.gy.signal = 0;
+W.gz.t = 0; W.gz.signal = 0;
+
+n = 1;    % row counter in ceq.loop
+nFirst = [];
+tic = 0;  % running timer marking start of segment instance (sec)
+
+while n < arg.blockRange(2) & tic - eps < min(ceq.duration, arg.timeRange(2))
+
+    % get segment index 
+    i = ceq.loop(n,1);  % segment index
+
+    % skip segment if needed
+    n1 = n;
+    n2 = n - 1 + ceq.segments(i).nBlocksInSegment;
+    if n < arg.blockRange(1);
+        n = n2 + 1;
+        continue;
+    end
+
+    if isempty(nFirst)
+        nFirst = n1;
+    end
+
+    % get dynamic (scan loop) information
+    L = ceq.loop(n:(n-1+ceq.segments(i).nBlocksInSegment), :);
+
+    % get segment instance duration
+    try
+        S = pge2.getsegmentinstance(ceq, i, sys, L, 'durationOnly', true);
+    catch ME
+        error(sprintf('(n = %d, i = %d): %s\n', n, i, ME.message));
+    end
+
+    if arg.timeRange(1) > tic + eps
+        % update time and skip to next segment instance
+        tic = tic + S.duration;
+        W.gx.t = tic; W.gx.signal = 0;
+        W.gy.t = tic; W.gy.signal = 0;
+        W.gz.t = tic; W.gz.signal = 0;
+        tStart = tic;
+        n = n + ceq.segments(i).nBlocksInSegment;
+        continue;
+    end
+
+    % Get segment instance and add to plot
+    try
+        S = pge2.getsegmentinstance(ceq, i, sys, L, 'rotate', arg.rotate, ...
+            'interpolate', arg.interpolate, 'wt', arg.wt);
+    catch ME
+        error(sprintf('(n = %d, i = %d): %s\n', n, i, ME.message));
+    end
+
+    sub_addSegmentInstanceToPlot(tic, S, arg.showBlocks, sys, yLim, nSubPlots);
+
+    % Add waveforms to running total (for return value -- not used for plotting)
+    W.tic = [W.tic(:); tic + S.tic(:)];
+
+    W.gx.t = [W.gx.t; tic + S.gx.t];
+    W.gy.t = [W.gy.t; tic + S.gy.t];
+    W.gz.t = [W.gz.t; tic + S.gz.t];
+
+    W.gx.signal = [W.gx.signal; S.gx.signal];
+    W.gy.signal = [W.gy.signal; S.gy.signal];
+    W.gz.signal = [W.gz.signal; S.gz.signal];
+
+    % Update time counter and go to next segment
+    tic = tic + S.duration;
+    n = n + ceq.segments(i).nBlocksInSegment;
+end
+
+% link axes
+for sp = 1:nSubPlots
+    subplot(nSubPlots, 1, sp); 
+    ax{sp} = gca;
+    grid on;
+end
+linkaxes([ax{1} ax{2} ax{3}], 'x');  % common zoom setting (along time axis) for all tiles
+
+% set misc figure properties
+subplot(nSubPlots,1,1);
+msg = sprintf('Displaying blocks %d:%d.\n', nFirst, n2);
+if ~arg.rotate
+    msg = sprintf('%sLogical coordinates -- gradient rotations not shown.\n', msg);
+end
+if arg.interpolate
+    msg = sprintf('%sGradient waveforms interpolated to 4us.', msg);
+else
+    msg = sprintf('%sWaveform samples/corner points are shown as defined in the original Pulseq file.', msg);
+end
+title(msg, 'FontSize', 24);
+
+return
+
+
+function sub_plotboundary(T, tp)
+    % T    [nt]    block boundary locations (time within sequence)
+    % tp   string  'block' or 'segment' 
+
+    if nargin < 2
+        tp = 'block';
+    end
+
+    T = unique(T);
+
+    hold on;
+    for m = 1:length(T)
+        if strcmp(tp, 'block')
+            xline(T(m), ':', 'linewidth', 1.2, 'color', 'k');
+        else
+            xline(T(m), '--', 'linewidth', 1.2, 'color', 'r');
+        end
+    end
+
+    return
+
+function sub_addSegmentInstanceToPlot(tic, S, showBlocks, sysGE, yLim, nSubPlots)
+    % tic  [1]      time of start of segment
+    % S    struct   segment instance
+
+    sp = 1;
+    cols = 'rgb';
+    for g = {'gx','gy','gz'}
+        % convert NaNs to zeros
+        S.(g{1}).signal(isnan(S.(g{1}).signal)) = 0;
+
+        subplot(nSubPlots,1,sp); hold on;
+        plot(1e3*(tic + S.(g{1}).t), S.(g{1}).signal, [cols(sp) '.-']);
+        ylabel({g{1}, 'G/cm'}, 'Rotation', 0, 'FontSize', 16);
+        yl = 1.05 * yLim.(g{1});
+        ylim(yl * [-1 1]);
+        if showBlocks
+            sub_plotboundary(1e3*(tic + S.tic));
+            sub_plotboundary(1e3*[tic + S.tic(1)], 'segment');
+        end
+        sp = sp + 1;
+    end
+
+    xlabel('time (ms)', 'FontSize', 16);
+
+    return
